9cd8fa6dfe151ac0b72c969442fcf60140baf47e
Lucas Cota <lucascdornelas@gmail.com>
2021-08-19 18:46:25 -0300

    refactor comments and name variables

--- dinamica/Dinamica.java
+++ dinamica/Dinamica.java
@@ -4,11 +4,11 @@ import util.LinhaDeMontagem;
 
 public class Dinamica {
 
-    private LinhaDeMontagem L1, L2; // linhas de montagem
-    private int[] tempoMin1, tempoMin2; // vetores para guardar os custos minimos
-    private int[] caminho1, caminho2; // vetores para guardar o caminho de quais linhas devem ser seguidas
+    private LinhaDeMontagem L1, L2; 
+    private int[] tempoMin1, tempoMin2; 
+    private int[] caminho1, caminho2;   // vetores para guardar o caminho de quais linhas devem ser seguidas
     private int tempoFinal, linhaFinal; // valores que armazenarao a saida otima
-    private int i; // variavel a ser iterada no metodo recursivo
+    private int i;                     
 
     public Dinamica(LinhaDeMontagem L1, LinhaDeMontagem L2) {
         this.tempoMin1 = new int[L1.getStationTime().length - 2];
@@ -16,79 +16,63 @@ public class Dinamica {
         this.caminho1 = new int[L1.getStationTime().length - 2];
         this.caminho2 = new int[L2.getStationTime().length - 2];
 
-        tempoMin1[0] = L1.getStationTime()[0] + L1.getStationTime()[1]; // inicializa a primeira posicao do primeiro vetor dos
-                                                                  // caminhos minimos
-        tempoMin2[0] = L2.getStationTime()[0] + L2.getStationTime()[1]; // inicializa a primeira posicao do segundo vetor dos
-                                                                  // caminhos minimos
+        // inicializa a primeira posicao do primeiro vetor dos caminhos minimos
+        tempoMin1[0] = L1.getStationTime()[0] + L1.getStationTime()[1];
+        tempoMin2[0] = L2.getStationTime()[0] + L2.getStationTime()[1]; 
 
-        caminho1[0] = 1; // inicializa a primeira posicao do vetor da ordem das linhas
-        caminho2[0] = 2; // inicializa a primeira posicao do vetor da ordem das linhas
+        caminho1[0] = 1; 
+        caminho2[0] = 2; 
 
         this.L1 = L1;
         this.L2 = L2;
         this.i = 1;
     }
 
-    // metodo dinamico para calcular o caminho minimo entre a entrada e saida
+    // método que calcula o caminho minimo entre a entrada e saida
     public void caminhoMinimo() {
-        // loop que comeca do 2 para que nao haja conflitos em arrays diferentes e de
-        // tamanhos diferentes
+        // loop que comeca do 2 para que nao haja conflitos em arrays diferentes e de tamanhos diferentes
         for (i = 2; i <= L1.getStationTime().length - 2; i++) {
 
-            // linha 1
-            // calcula o custo de se manter na estacao 1
+            // tempo para se manter na estacao 1 e 2
             int tempo1 = tempoMin1[i - 2] + L1.getStationTime()[i];
-            // calcula o custo de se trocar para a estacao 2
             int tempo2 = tempoMin2[i - 2] + L2.getTransportTime()[i - 2] + L1.getStationTime()[i];
 
             // compara qual dos dois custos e melhor para ser armazenado no vetor
             if (tempo1 <= tempo2) {
-                // armazena o custo 1 na posicao de memoria adequada do vetor custoMin1 (é a
-                // subsolucao otima)
+                // salva o tempo 1 que é a subsolucao otima e que o caminha contem a solução
                 tempoMin1[i - 1] = tempo1;
-                // armazena que a linha 1 e que contem a subsolucao otima
                 caminho1[i - 1] = 1;
             } else {
-                // armazena o custo 2 na posicao de memoria adequada do vetor custoMin1 (e a
-                // subsolucao otima)
+                // salva o tempo 2 que é a subsolucao otima e que o caminha contem a solução
                 tempoMin1[i - 1] = tempo2;
-                // armazena que a linha 2 e que contem a subsolucao otima
                 caminho1[i - 1] = 2;
             }
 
-            // linha 2
-            // calcula o custo de se manter na estacao 2
+            // tempo para se manter na estacao 1 e 2
             tempo1 = tempoMin2[i - 2] + L2.getStationTime()[i];
-            // calcula o custo de se trocar para a estacao 1
             tempo2 = tempoMin1[i - 2] + L1.getTransportTime()[i - 2] + L2.getStationTime()[i];
 
             // compara qual dos dois custos e melhor para ser armazenado no vetor
             if (tempo1 <= tempo2) {
-                // armazena o custo 1 na posicao de memoria adequada do vetor custoMin2 (e a
-                // subsolucao otima)
+                // salva o tempo 1 que é a subsolucao otima e que o caminha contem a solução
                 tempoMin2[i - 1] = tempo1;
-                // armazena que a linha 2 e que contem a subsolucao otima
                 caminho2[i - 1] = 2;
             } else {
-                // armazena o custo 2 na posio de memoria adequada do vetor custoMin2 (e a
-                // subsolucao otima)
+                // salva o tempo 2 que é a subsolucao otima e que o caminha contem a solução
                 tempoMin2[i - 1] = tempo2;
-                // armazena que a linha 1 e que contem a subsolucao otima
                 caminho2[i - 1] = 1;
             }
         }
 
-        // ao final calcula qual saida e otima
+        // calcula qual saida é otima
         if (tempoMin1[i - 2] + L1.getStationTime()[L1.getStationTime().length - 1] <= tempoMin2[i - 2]
                 + L2.getStationTime()[L2.getStationTime().length - 1]) {
-            // armazena a saida otima caso seja a primeira
+            // armazena a saida otima caso seja a primeira e registra que e na linha 1
             tempoFinal = tempoMin1[i - 2] + L1.getStationTime()[L1.getStationTime().length - 1];
-            // registra que e na linha 1
             linhaFinal = 1;
         } else {
-            // armazena a saida otima caso seja a primeira
+            // armazena a saida otima caso seja a primeira e registra que e na linha 2
             tempoFinal = tempoMin2[i - 2] + L2.getStationTime()[L2.getStationTime().length - 1];
-            // registra que e na linha 2
             linhaFinal = 2;
         }
 
--- main/Main.java
+++ main/Main.java
@@ -13,14 +13,15 @@ public class Main {
 
         System.out.println("Instancia - 1");
         
+        // Situação do problema 1
         int[] A1 = new int[] {3,5,7,10,5,9,11,9,5,2,6};
         int[] A2 = new int[] {2,6,3,9,11,4,9,3,12,4,5};
 
         int[] T1 = new int[] {3,5,4,2,7,5,8,1};
         int[] T2 = new int[] {5,3,7,5,6,2,5,2};
 
-        L1 = new LinhaDeMontagem(A1, T1); //criando a linha 1 
-        L2 = new LinhaDeMontagem(A2, T2); //criando a linha 2 
+        L1 = new LinhaDeMontagem(A1, T1); 
+        L2 = new LinhaDeMontagem(A2, T2);  
 
         System.out.println("\nGuloso: ");
 
@@ -31,16 +32,15 @@ public class Main {
 
         System.out.println("\nProgramacao Dinamica: \n");
 
-        // Aplicando o algoritmo dinamico para a primeira instancia
         programacaoDinamica = new Dinamica(L1, L2);
         programacaoDinamica.caminhoMinimo();
         programacaoDinamica.imprimeCaminhoMinimo();
 
         System.out.println("Instancia - 2");
 
+        // Situação do problema 2
         A1 = new int[] {5,10,6,3,8,5,3,7,12,8};
         A2 = new int[] {7,3,5,3,7,6,4,9,10,9};
-
         T1 = new int[] {4,2,7,2,5,8,2};
         T2 = new int[] {6,1,7,3,6,4,5};
 
@@ -49,14 +49,12 @@ public class Main {
 
         System.out.println("\nGuloso: ");
 
-        // Aplicando o algoritmo guloso para a segunda instancia
         guloso = new Guloso(L1, L2);
         guloso.gulosoMontagem();
         guloso.caminhoMinimoGuloso();
 
         System.out.println("\nProgramacao Dinamica: \n");
 
-        // Aplicando o algoritmo dinamico para a segunda instancia
         programacaoDinamica = new Dinamica(L1, L2);
         programacaoDinamica.caminhoMinimo();
         programacaoDinamica.imprimeCaminhoMinimo();
